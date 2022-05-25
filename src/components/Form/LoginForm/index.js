53d009008c890967558c08a09ac68749d04ff598
Valmy Ericles Nunes Machado <valmyericlesnunesmachado@MacBook-Air-de-Valmy.local>
2022-05-25 13:38:07 -0300

    btn login github

--- src/components/Form/LoginForm/index.js
+++ src/components/Form/LoginForm/index.js
@@ -2,14 +2,44 @@ import { Divider } from "../Divider"
 import { InputBase } from "../Input"
 import { LoginButton } from "../Buttons/LoginButton"
 import { Container, ButtonsWrapper } from "./styles"
+import { api } from '../../../services/api'
+
+import LoginGithub from 'react-login-github';
 
 export const LoginForm = () => {
+
+  const onSuccess = async response => {
+    try {
+
+      const client_id = process.env.REACT_APP_GITHUB_CLIENT_ID
+      const client_secret = process.env.REACT_APP_GITHUB_CLIENT_SECRET
+      
+      const url = `/login/oauth/access_token?client_id=${client_id}&client_secret=${client_secret}&codigo=${response.code}`
+      // const resp = await api.post(url, '', { headers: {
+      //   'Access-Control-Allow-Origin': '*'
+      // }});
+
+      const resp = await api.post(url, '', { headers: {
+        "Access-Control-Allow-Origin": "*",
+        // client_id,
+        // client_secret,
+        // codigo: response.code,
+      }});
+
+      console.log(resp)
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
+  const onFailure = response => console.error(response);
+
   return (
     <Container>
       <span>Login</span>
-      
-      <InputBase type="text" placeholder="Digite seu email" label="Email"/>
-      <InputBase type="password" placeholder="Digite sua senha" label="Senha"/>
+
+      <InputBase type="text" placeholder="Digite seu email" label="Email" />
+      <InputBase type="password" placeholder="Digite sua senha" label="Senha" />
 
 
       <ButtonsWrapper>
@@ -17,8 +47,15 @@ export const LoginForm = () => {
           Entrar
         </LoginButton>
 
-        <Divider/>
-        
+        <Divider />
+
+        <LoginGithub
+          clientId={process.env.REACT_APP_GITHUB_CLIENT_ID}
+          redirectUri={process.env.REACT_APP_GITHUB_REDIRECT_URL}
+          onSuccess={onSuccess}
+          onFailure={onFailure}
+        />
+
         <LoginButton isGithub>
           Entrar com Github
         </LoginButton>
--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { LoginForm } from "../../components/Form/LoginForm";
 import { Container } from "./styles";
 
 const Login = () => {
+
+  // useEffect(() => {
+  //   const queryString = window.location.search;
+  //   const urlParams = new URLSearchParams(queryString)
+  //   const codeObj = urlParams.get('code')
+
+    
+
+  //   if(codeObj?.code) getAccessTokenGithub(codeObj.code)
+  // },[])
+
   return (
     <Container>
       <LoginForm/>
