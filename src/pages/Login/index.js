2e881b55cd27d51a96b8d45b1cfaf194ec53efd7
Valmy Ericles Nunes Machado <valmyericlesnunesmachado@MacBook-Air-de-Valmy.local>
2022-05-28 09:05:54 -0300

    remove comment

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,43 +1,8 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { LoginForm } from "../../components/LoginForm";
 import { Container } from "./styles";
-import { api } from '../../services/api'
 
 const Login = () => {
-
-  // useEffect(() => {
-  //   const queryString = window.location.search;
-  //   const urlParams = new URLSearchParams(queryString)
-  //   const code = urlParams.get('code')
-    
-
-  //   const client_id = process.env.REACT_APP_GITHUB_CLIENT_ID
-  //   const client_secret = process.env.REACT_APP_GITHUB_CLIENT_SECRET
-
-  //   async function getAccessToken() {
-
-  //     const body = {
-  //       client_id,
-  //       client_secret,
-  //       code,
-  //     }
-
-  //     try {
-  //       const resp = await api.post('/login/oauth/access_token', body, {
-  //         headers: {
-  //           "Access-Control-Allow-Origin": "*",
-  //           "Accept": "application/json"
-  //         }
-  //       });
-  //       console.log(resp)
-  //     } catch (err) {
-  //       console.log(err)
-  //     }
-  //   }
-
-  //   if(code) getAccessToken(code)
-  // }, [])
-
   return (
     <Container>
       <LoginForm />
